4388f5877c6976d1fabc442cb97f686a076a5a0d
wongjoshua2005 <69170386+wongjoshua2005@users.noreply.github.com>
2024-06-26 22:03:14 -0400

    Added the very basics of a grid layout
    I have implemented the basics of a 16x16 grid layout. It's not
    perfect, but I made some progress.

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-// Container needed for the Etch A Sketch
-const container = document.createElement("div");
-container.style.display = "flex";
-container.style.alignItems = "center";
-container.style.justifyContent = "center";
+function createGrid(size, storeGrids)
+{
+    for (let i = 0; i < size; i++)
+    {
+        const squareGrid = document.createElement("div");
+        squareGrid.setAttribute("class", "grid");
+        squareGrid.style.backgroundColor = "white";
+        squareGrid.style.border = "0.5px solid";
+        squareGrid.style.padding = "15px";
+        squareGrid.style.textAlign = "center";  
+        storeGrids.appendChild(squareGrid);
+    }
+}
+
+function initialSetup()
+{
+    // To make the background-color be filling the the whole body
+    document.body.style.backgroundColor =  "lightblue";
+
+    // Container needed for the Etch A Sketch
+    const totalGrids = 256;
+
+    const container = document.createElement("div");
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = "repeat(16, 35px)";
+    container.style.alignItems = "center";
+    container.style.justifyContent = "center";
+
+    document.body.appendChild(container);
+
+    createGrid(totalGrids, container);
+}
+
+initialSetup();
