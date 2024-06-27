c7b143bbc736a775b9fe803a71dceec5a2c4bf46
wongjoshua2005 <69170386+wongjoshua2005@users.noreply.github.com>
2024-06-26 20:39:49 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Etch-a-Sketch
+
+This project will create the basics of the Etch A Sketch that will test my
+abilities in using DOM.
---
+++ script.js
@@ -0,0 +1,5 @@
+// Container needed for the Etch A Sketch
+const container = document.createElement("div");
+container.style.display = "flex";
+container.style.alignItems = "center";
+container.style.justifyContent = "center";
